d50b0fc221a07867711b5ddcf4ee814233b43e94
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2024-05-21 13:21:54 +0100

    Update loadfile_source.lua

--- scripts/reference/loadfile_source.lua
+++ scripts/reference/loadfile_source.lua
@@ -1,6 +1,3 @@
-assert(loadfile("E:\\GitHub\\FUN-MAP_NTTR\\scripts\\Fun-Map_NTTR.lua"))()
+assert(loadfile("D:\\GitHub\\JTF1-NO_MODS_CAUCASUS\\scripts\\dynamic\\dynamic_loader.lua"))()
 
-
-assert(loadfile("E:\\GitHub\\FUN-MAP_NTTR\\scripts\\Fun-Map_NTTR_DEV.lua"))()
-
-assert(loadfile("E:/GitHub/FAM-FLIGHT_NTTR/scripts/dynamic/dynamic_loader.lua"))()
+'D:\\GitHub\\NO_MODS_CAUCASUS\\scripts\\dynamic\\'
